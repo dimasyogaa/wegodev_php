d979de1d0bbc5e6d315d973534041d1227bb27d4
Yogadimas <yogadimaspambudi@gmail.com>
2023-07-30 21:40:00 +0700

    Post

---
+++ _11_Post/a_post.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Post</title>
+</head>
+
+<body>
+
+    <form action="b_post_tangkap.php" method="post">
+
+        Nama :
+        <input type="text" name="nama">
+
+        <br />
+
+        <input type="submit" value="kirim">
+
+    </form>
+
+</body>
+
+</html>
