842ed376f06b52f0b3e1e7068f15e66f51084681
Yogadimas <yogadimaspambudi@gmail.com>
2023-07-30 21:24:14 +0700

    Array

---
+++ _9_Array/array.php
@@ -0,0 +1,30 @@
+<?php
+
+// Membuat Array
+
+//Cari 1
+$nama[0] = "Rudi";
+$nama[1] = "Desta";
+$nama[2] = "Mike";
+
+// Cara 2
+$jenisKelamin = array(
+    "C" => "Cewek",
+    "L" => "Laki-Laki"
+);
+
+// Cara 3
+$olahraga = array("Bola", "Basket", "Badminton", "Tenis Meja");
+
+// Cara menampilkan array
+
+echo $nama[1];
+
+echo "<br/>";
+
+echo $jenisKelamin["C"];
+
+echo "<br/>";
+
+echo $olahraga[2];
+
