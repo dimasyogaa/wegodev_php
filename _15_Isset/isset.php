b1f30f9b04b0f75cd1889eefabe7274973f3bc0d
Yogadimas <yogadimaspambudi@gmail.com>
2023-07-31 23:55:00 +0700

    Isset

---
+++ _15_Isset/isset.php
@@ -0,0 +1,16 @@
+<?php
+
+$buah = "Apel";
+$kendaraan = "Mobil";
+
+echo "Hasil Variable Buah :" . isset($buah); // 1 (true)
+
+echo "<br/>";
+
+echo "Hasil Variable Kendaraan :" . isset($kendaraan); // 1 (true)
+
+echo "<br/>";
+
+echo "Hasil Variable Minuman :" . isset($minuman); // tak bernilai (false)
+
+
